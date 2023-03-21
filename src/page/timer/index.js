9b93e370b2e2996a2419272f1d8e13eeed3a54f1
kangyite <kangyte@gmail.com>
2023-03-21 15:19:46 +0800

    commit

---
+++ src/page/timer/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import {} from "./style.css";
+import { db } from "../../firebase";
+import { onValue, ref } from "firebase/database";
+import { useNavigate } from "react-router-dom";
+class Timer extends Component {
+	state = {
+		timerData: [],
+	};
+	componentDidMount() {
+		this.id = this.props.id;
+		console.log(this.props);
+	}
+	render() {
+		return <h1>asd</h1>;
+	}
+}
+function WithNavigate(props) {
+	let navigate = useNavigate();
+	return <Timer {...props} navigate={navigate} />;
+}
+
+export default WithNavigate;

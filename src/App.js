df0ffe7c91a02cc0e1501bc56464615f6a8909a6
kangyite <kangyte@gmail.com>
2023-03-22 17:01:10 +0800

    added some stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 // import { firebase } from "./firebase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { Routes, Route, useNavigate } from "react-router-dom";
@@ -8,54 +8,32 @@ import Signup from "./page/signup";
 import Login from "./page/login";
 import Timer from "./page/timer";
 
-class App extends Component {
-	state = {};
-
-	componentDidMount() {
+const App = () => {
+	const navigate = useNavigate();
+	useEffect(() => {
 		const auth = getAuth();
 		onAuthStateChanged(auth, (user) => {
 			if (user) {
 				console.log("signed in");
-				this.props.navigate("/");
+				navigate("/");
 			} else {
 				console.log("Not signed in");
-				this.props.navigate("/login");
+				navigate("/login");
 			}
 		});
-		window.addEventListener("showRefreshSnack", this.showRefreshSnack);
-	}
-	componentWillUnmount() {
-		window.removeEventListener("showRefreshSnack", this.showRefreshSnack);
-		if (this.detachSiteInfo) {
-			this.detachSiteInfo();
-		}
-	}
-	showRefreshSnack = () => {
-		this.snackbar.MDComponent.show({
-			message: "Site updated. Refresh this page for better experience.",
-			actionText: "Refresh",
-			timeout: 5000,
-			actionHandler: () => {
-				console.log("oi");
-				window.location.reload();
-			},
-		});
-	};
-	render() {
-		return (
-			<div>
-				<Routes>
-					<Route path="/" Component={Home} />
-					<Route path="/signup" Component={Signup} />
-					<Route path="/login" Component={Login} />
-					<Route path="/timer/:id" Component={Timer} />
-				</Routes>
-			</div>
-		);
-	}
-}
-function WithNavigate(props) {
-	let navigate = useNavigate();
-	return <App {...props} navigate={navigate} />;
-}
-export default WithNavigate;
+		// eslint-disable-next-line
+	}, []);
+
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/timer/:id" element={<Timer />} />
+			</Routes>
+		</div>
+	);
+};
+
+export default App;
--- src/page/timer/index.js
+++ src/page/timer/index.js
@@ -1,23 +1,119 @@
-import React, { Component } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {} from "./style.css";
-// import { db } from "../../firebase";
-// import { onValue, ref } from "firebase/database";
-import { useNavigate } from "react-router-dom";
-class Timer extends Component {
-	state = {
-		timerData: [],
-	};
-	componentDidMount() {
-		this.id = this.props.id;
-		console.log(this.props);
-	}
-	render() {
-		return <h1>asd</h1>;
-	}
-}
-function WithNavigate(props) {
-	let navigate = useNavigate();
-	return <Timer {...props} navigate={navigate} />;
-}
+import { db } from "../../firebase";
+import { onValue, ref, set } from "firebase/database";
+import { useNavigate, useParams } from "react-router-dom";
 
-export default WithNavigate;
+import Editable from "../../components/Editable";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+const path = window.location.pathname;
+const timerId = path.substring(path.lastIndexOf("/") + 1, path.length);
+console.log(timerId);
+let changed = false;
+const Alert = React.forwardRef(function Alert(props, ref) {
+	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+const Timer = () => {
+	const inputRef = useRef();
+	const [openSnack, setOpenSnack] = React.useState(false);
+	const [timerData, setTimerData] = useState({
+		actual: 0,
+		enable_ot: 0,
+		name: "",
+		online: false,
+		ot_time: "",
+		plan: 0,
+		target: 0,
+		working_time: "",
+	});
+	const changeHandler = (e) => {
+		setTimerData({ ...timerData, [e.target.name]: e.target.value });
+		changed = true;
+		console.log(changed);
+	};
+	const handleUpdate = () => {
+		set(ref(db, `devices/TIMER_1/data`), {
+			...timerData,
+		})
+			.then(() => {
+				setOpenSnack(true);
+			})
+			.catch((error) => {
+				// The write failed...
+			});
+	};
+
+	const handleCloseSnack = (event, reason) => {
+		if (reason === "clickaway") {
+			return;
+		}
+
+		setOpenSnack(false);
+	};
+	useEffect(() => {
+		const query = ref(db, `devices/${timerId}`);
+		console.log(`devices/${timerId}`);
+		onValue(query, (snapshot) => {
+			if (snapshot.exists()) {
+				let timerData = [];
+				const snapshotData = snapshot.val();
+				timerData = snapshotData.data;
+
+				// setTimerData(timerData);
+				Object.keys(timerData).forEach((key) => {
+					setTimerData({ ...timerData });
+				});
+			}
+		});
+	}, []);
+	return (
+		<div className="page">
+			<div className={"page_title_container"}>
+				<h1>{timerData.name || "Unnamed"} </h1>
+				<div className="data_title_container">
+					<h4>Plan: </h4>
+					<Editable
+						text={timerData.plan}
+						placeholder="Write a value"
+						childRef={inputRef}
+						type="input"
+					>
+						<input
+							ref={inputRef}
+							type="number"
+							name="plan"
+							placeholder="Write a value"
+							value={timerData.plan}
+							onChange={changeHandler}
+						/>
+					</Editable>
+				</div>
+				<p>
+					{changed
+						? "Kindly press the Update button for updating database."
+						: ""}
+				</p>
+				<Button variant="contained" onClick={handleUpdate}>
+					Update
+				</Button>
+				<Snackbar
+					open={openSnack}
+					autoHideDuration={5000}
+					onClose={handleCloseSnack}
+				>
+					<Alert
+						onClose={handleCloseSnack}
+						severity="success"
+						sx={{ width: "100%" }}
+					>
+						Updated to Firebase!
+					</Alert>
+				</Snackbar>
+			</div>
+		</div>
+	);
+};
+
+export default Timer;

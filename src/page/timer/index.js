4e645b21ad492fe40f94c34a46e1bc48fc6cf667
kangyite <kangyte@gmail.com>
2023-05-25 15:32:03 +0800

    replace editable to datagrid

--- src/page/timer/index.js
+++ src/page/timer/index.js
@@ -3,23 +3,38 @@ import {} from "./style.css";
 import { db } from "../../firebase";
 import { onValue, ref, set, update } from "firebase/database";
 
-import Editable from "../../components/Editable";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
-import Switch from "@mui/material/Switch";
 import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
+import { DataGrid } from "@mui/x-data-grid";
 let changed = false;
 const Alert = React.forwardRef(function Alert(props, ref) {
 	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
+
+const useFakeMutation = () => {
+	return React.useCallback(
+		(obj) =>
+			new Promise((resolve, reject) => {
+				setTimeout(() => {
+					if (obj.value.length === 0) {
+						reject(new Error("Error while saving, value can't be empty."));
+					} else {
+						resolve({ ...obj });
+					}
+				}, 200);
+			}),
+		[]
+	);
+};
+
 const Timer = () => {
-	const inputRef = useRef();
 	const [openSnack, setOpenSnack] = React.useState(false);
 	const [timerData, setTimerData] = useState({
 		actual: 0,
@@ -58,22 +73,6 @@ const Timer = () => {
 
 		// eslint-disable-next-line
 	}, [timerData.name]);
-	const handleOTChange = (e) => {
-		setTimerData({
-			...timerData,
-			[e.target.name]: e.target.checked,
-		});
-		changed = true;
-	};
-	const changeHandler = (e) => {
-		setTimerData({
-			...timerData,
-			[e.target.name]:
-				e.target.type === "number" ? Number(e.target.value) : e.target.value,
-		});
-		changed = true;
-		console.log(timerData);
-	};
 	const handleUpdate = () => {
 		set(ref(db, `devices/TIMER_${timerData.id}/data`), {
 			...timerData,
@@ -116,6 +115,78 @@ const Timer = () => {
 		});
 		// eslint-disable-next-line
 	}, []);
+
+	const dataCol = [
+		{ field: "data_name", headerName: "Data", width: 150, sortable: false },
+		{
+			field: "value",
+			headerName: "Value",
+			width: 400,
+			sortable: false,
+			editable: true,
+		},
+	];
+	const dataRow = [
+		{ id: 0, data: "timer_set", data_name: "Timer Set", value: timerData.timer_set}, //prettier-ignore
+		{ id: 1, data: "plan", data_name: "Plan	", value: timerData.plan },
+		{ id: 2, data: "target", data_name: "Target", value: timerData.target },
+		{ id: 3, data: "actual", data_name: "Actual", value: timerData.actual },
+		{ id: 4, data: "working_time", data_name: "Working Time", value: timerData.working_time }, //prettier-ignore
+		{ id: 5, data: "ot_time", data_name: "OT Time", value: timerData.ot_time }, //prettier-ignore
+		{ id: 6, data: "enable_ot", data_name: "Enable OT", value: timerData.enable_ot }, //prettier-ignore
+	];
+	const mutateRow = useFakeMutation();
+
+	const processRowUpdate = React.useCallback(
+		async (newRow) => {
+			// Make the HTTP request to save in the backend
+			const response = await mutateRow(newRow);
+			let data = String(response.data);
+			let value = response.value;
+			if (
+				data === "timer_set" ||
+				data === "plan" ||
+				data === "target" ||
+				data === "actual"
+			) {
+				value = Number(value);
+				if (value >= 10000 || value < 0) {
+					return Promise.reject("Data out of range.");
+				}
+			} else if (data === "working_time" || data === "ot_time") {
+				for (let i = 0; i < value.length; i++) {
+					if ((i + 6) % 10 === 0)
+						if (value[i] !== "-") return Promise.reject("3Wrong format!");
+					if ((i + 1) % 10 === 0)
+						if (value[i] !== ",") return Promise.reject("2Wrong format!");
+				}
+				if((value.match(/,/g)||[]).length !== (value.match(/-/g)||[]).length - 1) return Promise.reject("1Wrong format!"); //prettier-ignore
+				let temp = value.split(",");
+				for (let i in temp) {
+					let fT = temp[i].split("-");
+					if (fT[0] > fT[1]) return Promise.reject("Wrong format!");
+					if (fT[0].substring(0, 2) < 0 || fT[0].substring(2, 4) > 59)
+						return Promise.reject("Wrong format!");
+					if (fT[1].substring(0, 2) < 0 || fT[1].substring(2, 4) > 59)
+						return Promise.reject("Wrong format!");
+				}
+			} else if (data === "enable_ot") {
+				if (value === "true") value = true;
+				else if (value === "false") value = false;
+				else return Promise.reject("Please write true/false");
+			}
+
+			setTimerData({ ...timerData, [data]: value });
+			return response;
+		},
+		[mutateRow, timerData]
+	);
+	const handleProcessRowUpdateError = React.useCallback((error) => {
+		setSnackbar({ children: error.message || error, severity: "error" });
+	}, []);
+	const [snackbar, setSnackbar] = React.useState(null);
+
+	const handleCloseSnackbar = () => setSnackbar(null);
 	return (
 		<div className="page">
 			<div className={"page_title_container"}>
@@ -144,123 +215,42 @@ const Timer = () => {
 						<Button onClick={handleSubmitName}>Submit</Button>
 					</DialogActions>
 				</Dialog>
-				<div className="data_title_container">
-					<h4>Timer Set: </h4>
-					<Editable
-						text={timerData.timer_set}
-						placeholder="Write a value"
-						childRef={inputRef}
-						type="input"
-					>
-						<input
-							ref={inputRef}
-							type="number"
-							name="timer_set"
-							placeholder="Write a value"
-							value={timerData.timer_set}
-							onChange={changeHandler}
-						/>
-					</Editable>
-				</div>
-				<div className="data_title_container">
-					<h4>Plan: </h4>
-					<Editable
-						text={timerData.plan}
-						placeholder="Write a value"
-						childRef={inputRef}
-						type="input"
-					>
-						<input
-							ref={inputRef}
-							type="number"
-							name="plan"
-							placeholder="Write a value"
-							value={timerData.plan}
-							onChange={changeHandler}
-						/>
-					</Editable>
-				</div>
-				<div className="data_title_container">
-					<h4>Target: </h4>
-					<Editable
-						text={timerData.target}
-						placeholder="Write a value"
-						childRef={inputRef}
-						type="input"
-					>
-						<input
-							ref={inputRef}
-							type="number"
-							name="target"
-							placeholder="Write a value"
-							value={timerData.target}
-							onChange={changeHandler}
-						/>
-					</Editable>
-				</div>
-				<div className="data_title_container">
-					<h4>Actual: </h4>
-					<Editable
-						text={timerData.actual}
-						placeholder="Write a value"
-						childRef={inputRef}
-						type="input"
-					>
-						<input
-							ref={inputRef}
-							type="number"
-							name="actual"
-							placeholder="Write a value"
-							value={timerData.actual}
-							onChange={changeHandler}
-						/>
-					</Editable>
-				</div>
-				<div className="data_title_container">
-					<h4>Working Time: </h4>
-					<Editable
-						text={timerData.working_time}
-						placeholder="Write a value"
-						childRef={inputRef}
-						type="input"
-					>
-						<input
-							ref={inputRef}
-							type="text"
-							name="working_time"
-							placeholder="Write a value"
-							value={timerData.working_time}
-							onChange={changeHandler}
-						/>
-					</Editable>
-				</div>
-				<div className="data_title_container">
-					<h4>OT Time: </h4>
-					<Editable
-						text={timerData.ot_time}
-						placeholder="Write a value"
-						childRef={inputRef}
-						type="input"
-					>
-						<input
-							ref={inputRef}
-							type="text"
-							name="ot_time"
-							placeholder="Write a value"
-							value={timerData.ot_time}
-							onChange={changeHandler}
-						/>
-					</Editable>
-				</div>
-
-				<div className="data_title_container">
-					<h4>Enable OT: </h4>
-					<Switch
-						checked={timerData.enable_ot}
-						name="enable_ot"
-						onChange={handleOTChange}
-						inputProps={{ "aria-label": "controlled" }}
+				<div style={{ height: "422px", width: "100%", paddingTop: "25px" }}>
+					<DataGrid
+						rows={dataRow}
+						columns={dataCol}
+						initialState={{
+							pagination: {
+								paginationModel: {
+									pageSize: 10,
+								},
+								sorting: {
+									sortModel: [{ field: "id", sort: "asc" }],
+								},
+							},
+						}}
+						pageSizeOptions={[1, 2]}
+						disableRowSelectionOnClick
+						disableColumnMenu
+						autoPageSize
+						hideFooter
+						hideFooterSelectedRowCount
+						isCellEditable={(params) =>
+							!["actual", "target"].includes(params.row.data)
+						}
+						processRowUpdate={processRowUpdate}
+						onProcessRowUpdateError={handleProcessRowUpdateError}
 					/>
+					{!!snackbar && (
+						<Snackbar
+							open
+							anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+							onClose={handleCloseSnackbar}
+							autoHideDuration={6000}
+						>
+							<Alert {...snackbar} onClose={handleCloseSnackbar} />
+						</Snackbar>
+					)}
 				</div>
 
 				<p>
